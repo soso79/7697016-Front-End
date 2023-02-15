6999c4f36d44adaa12a15e1abde4f28fd970c09d
sofiane bahri <bahrisofiane79@gmail.com>
2023-02-15 15:58:43 +0100

    essai vignettes

--- pieces.js
+++ pieces.js
@@ -28,3 +28,21 @@ sectionFiches.appendChild(categorieElement);
 sectionFiches.appendChild(descriptionElement);
 sectionFiches.appendChild(stockElement);
 
+for (let i = 0; i < pieces.length; i++) {
+
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // On crée l’élément img.
+    const imageElement = document.createElement("img");
+    // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
+    imageElement.src = pieces[i].image;
+    // Idem pour le nom, le prix et la catégorie...
+    
+    // On rattache la balise article à la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    // Idem pour le nom, le prix et la catégorie...
+    }
