74e29a4384ed076010396cb612d07bea8d8668c8
sofiane bahri <bahrisofiane79@gmail.com>
2023-02-15 12:33:01 +0100

    mise a jour

--- pieces.js
+++ pieces.js
@@ -8,13 +8,23 @@ imageElement.src = article.image;
 const nomElement = document.createElement("h2");
 nomElement.innerText = article.nom;
 const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} €`;
+prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie;
+categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+//Création des élements pour l'exercice
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+const stockElement = document.createElement("p");
+stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
+
+
 //Rattachement au fichier parent Fiches
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(stockElement);
 
