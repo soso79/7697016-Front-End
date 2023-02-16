3038c710299bade81a5ab44d0cb22f3564dc70cd
sofiane bahri <bahrisofiane79@gmail.com>
2023-02-16 12:28:07 +0100

    on fait apparaitre tous les éléments

--- pieces.js
+++ pieces.js
@@ -1,10 +1,15 @@
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
+
+for (let i = 0; i < pieces.length; i++){
+
+
 // Création d éléments avec document.createElement
-const article = pieces[0];
+const article = pieces[i];
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
+const pieceElement = document.createElement("article")
 const nomElement = document.createElement("h2");
 nomElement.innerText = article.nom;
 const prixElement = document.createElement("p");
@@ -21,28 +26,12 @@ stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock"
 
 //Rattachement au fichier parent Fiches
 const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
+sectionFiches.appendChild(pieceElement);
+pieceElement.appendChild(nomElement);
+pieceElement.appendChild(prixElement);
+pieceElement.appendChild(categorieElement);
+pieceElement.appendChild(descriptionElement);
+pieceElement.appendChild(stockElement);
+pieceElement.appendChild(imageElement);
 
-for (let i = 0; i < pieces.length; i++) {
-
-    // Récupération de l'élément du DOM qui accueillera les fiches
-    const sectionFiches = document.querySelector(".fiches");
-    // Création d’une balise dédiée à une pièce automobile
-    const pieceElement = document.createElement("article");
-    // On crée l’élément img.
-    const imageElement = document.createElement("img");
-    // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
-    imageElement.src = pieces[i].image;
-    // Idem pour le nom, le prix et la catégorie...
-    
-    // On rattache la balise article à la section Fiches
-    sectionFiches.appendChild(pieceElement);
-    // On rattache l’image à pieceElement (la balise article)
-    pieceElement.appendChild(imageElement);
-    // Idem pour le nom, le prix et la catégorie...
-    }
+}
