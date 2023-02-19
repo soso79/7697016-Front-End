421dd528c3ba51d8f6ffb9b079296d404d6de36e
sofiane bahri <bahrisofiane79@gmail.com>
2023-02-19 14:18:53 +0100

    exercice

--- pieces.js
+++ pieces.js
@@ -58,7 +58,7 @@ boutonFiltrer.addEventListener("click",function() {
     console.log(piecesFiltrees)
 
 });
-
+//bouton pour classer en ordre decroissant
 const boutonDecroissant = document.querySelector(".btn-decroissant");
 
 boutonDecroissant.addEventListener("click", function () {
@@ -68,6 +68,7 @@ boutonDecroissant.addEventListener("click", function () {
     });
     console.log(piecesOrdonnees);
 });
+// bouton no description
 const boutonNodescription = document.querySelector(".btn-nodescription");
 
 boutonNodescription.addEventListener("click", function () {
@@ -79,3 +80,22 @@ boutonNodescription.addEventListener("click", function () {
     
     console.log(piecesFiltrees)
 });
+
+const noms = pieces.map(piece => piece.nom);
+for(let i = pieces.length -1 ; i>=0; i--){
+    if(pieces[i].prix > 35){
+        noms.splice(i,1);
+    }
+}
+//creation liste
+const abordablesElements = document.createElement('ul');
+//ajout de chaque nom a la liste
+for(let i=0; i < noms.lenght ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement)
+}
+//ajout de l en-téte puis de la liste au bloc résultats
+
+document.querySelector('.abordables')
+    .appendChild(abordablesElements)
